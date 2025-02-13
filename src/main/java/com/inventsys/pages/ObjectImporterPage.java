a2f81e474c91c63e2e32e055f63abfa5282c2738
Gilson Souza <juniorgrms44@gmail.com>
2025-02-13 10:25:49 +0000

    adjusting commits.

--- src/main/java/com/inventsys/pages/ObjectImporterPage.java
+++ src/main/java/com/inventsys/pages/ObjectImporterPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class ObjectImporterPage extends BasePage {
 
     private static final Logger logger = LoggerFactory.getLogger(ObjectImporterPage.class);
